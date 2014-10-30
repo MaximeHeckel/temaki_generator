97a976526272a914816773090542bedd1d7611fe
koheku <phkoeck@gmail.com>
2014-10-30 12:36:14 +0100

    Add logs

--- src/generator.go
+++ src/generator.go
@@ -1,58 +1,74 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "math/rand"
-    "strings"
-    "os/exec"
+	"fmt"
+	"math/rand"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 func main() {
-  rand.Seed(time.Now().UTC().UnixNano())
-  consonne := []string{"h","k","h","n","m","y","d","r"}
-  voyelle := []string{"a","e","i","o","u"}
-  s := constructString(consonne,voyelle)
-  fmt.Println(s)
-  s = s + ".com"
-  fmt.Println(s)
-  /*cmd := exec.Command("host",s)
-  output, err := cmd.Output()
-  if err != nil {
-    fmt.Println(err)
-  } else {
-    fmt.Println(string(output))
-  }*/
-  executeHostCommand(s)
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	cnt := 0
+	domains := []string{}
+	for cnt < 10 {
+		domains = append(domains, findAvailableDomainName())
+		cnt++
+	}
+	fmt.Print("Here's 10 available domains ! ")
+	for i := 0; i < len(domains); i++ {
+		fmt.Print(domains[i] + " ")
+	}
+}
+
+func findAvailableDomainName() string {
+	available := false
+	result := ""
+	for !available {
+		consonne := []string{"h", "k", "h", "n", "m", "y", "d", "r"}
+		voyelle := []string{"a", "e", "i", "o", "u"}
+		s := constructString(consonne, voyelle)
+		fmt.Print("testing " + s + "... ")
+		available = executeHostCommand(s + ".com")
+		if available {
+			fmt.Println("available !")
+		} else {
+			fmt.Println("not available.")
+		}
+		result = s
+	}
+	return result
+}
+
+func constructString(array1 []string, array2 []string) string {
+	s := []string{}
+	for i := 0; i < 3; i++ {
+		c1 := pickRandomLetter(array1)
+		c2 := pickRandomLetter(array2)
+		s = append(s, c1)
+		s = append(s, c2)
+	}
+	str := strings.Join(s, "")
+	return str
 }
 
 func randInt(min int, max int) int {
-    return min + rand.Intn(max-min)
+	return min + rand.Intn(max-min)
 }
 
 func pickRandomLetter(array []string) string {
-  return array[randInt(1,len(array))]
+	return array[randInt(1, len(array))]
 }
 
-func constructString(array1 []string, array2 []string) string{
-  s := []string{}
-  for i:=0; i<3; i++{
-     c1 := pickRandomLetter(array1)
-     c2 := pickRandomLetter(array2)
-     s = append(s,c1)
-     s = append(s,c2)
-  }
-  str := strings.Join(s,"")
-  return str
-}
+func executeHostCommand(s string) bool {
+	cmd := exec.Command("host", s)
+	_, err := cmd.Output()
 
-func executeHostCommand(s string) string {
-  cmd := exec.Command("host",s)
-  output, err := cmd.Output()
-  if err != nil {
-    fmt.Println(err)
-    return "error"
-  } else {
-    return string(output)
-  }
+	if err != nil {
+		return true
+	} else {
+		return false
+	}
 }
