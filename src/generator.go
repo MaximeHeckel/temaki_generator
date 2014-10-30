81da1bd1301789b86840f33d2bb5f10ec833d03c
koheku <phkoeck@gmail.com>
2014-10-30 12:46:45 +0100

    Fix formatting

--- src/generator.go
+++ src/generator.go
@@ -23,31 +23,30 @@ func main() {
 }
 
 func findAvailableDomainName() string {
+	consonants := []string{"h", "k", "h", "n", "m", "y", "d", "r"}
+	vowels := []string{"a", "e", "i", "o", "u"}
+	domainName := ""
 	available := false
-	result := ""
+
 	for !available {
-		consonne := []string{"h", "k", "h", "n", "m", "y", "d", "r"}
-		voyelle := []string{"a", "e", "i", "o", "u"}
-		s := constructString(consonne, voyelle)
-		fmt.Print("testing " + s + "... ")
-		available = executeHostCommand(s + ".com")
+		domainName = constructString(consonants, vowels)
+		fmt.Print("testing " + domainName + "... ")
+		available = executeHostCommand(domainName + ".com")
 		if available {
 			fmt.Println("available !")
 		} else {
 			fmt.Println("not available.")
 		}
-		result = s
 	}
-	return result
+
+	return domainName
 }
 
 func constructString(array1 []string, array2 []string) string {
 	s := []string{}
 	for i := 0; i < 3; i++ {
-		c1 := pickRandomLetter(array1)
-		c2 := pickRandomLetter(array2)
-		s = append(s, c1)
-		s = append(s, c2)
+		s = append(s, pickRandomLetter(array1))
+		s = append(s, pickRandomLetter(array2))
 	}
 	str := strings.Join(s, "")
 	return str
