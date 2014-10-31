4c51d65ad3a9e777e19328f2ea7d52a0c899bbea
koheku <phkoeck@gmail.com>
2014-10-31 13:56:01 +0100

    Update checking
    . Add NameChecker
    . Add checkers.go

--- cli/cli.go
+++ cli/cli.go
@@ -21,124 +21,96 @@ var sites = []string{
 	"https://facebook.com/%s",
 }
 
-var domains chan string
-var httpDomains chan string
-var dnsDomains chan string
+var namesGenerator chan string
 
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	httpDomains = make(chan string, 1)
-	dnsDomains = make(chan string, 1)
-	domains = make(chan string)
-
-	go func() {
-		for domain := range domains {
-			httpDomains <- domain
-			dnsDomains <- domain + ".com"
-		}
-		close(httpDomains)
-		close(dnsDomains)
-	}()
-
-	go func() {
-		for i := 0; i < 100; i++ {
-			domains <- generator.ConstructString(consonants, vowels)
-		}
-
-		close(domains)
-	}()
-}
-
-type FreeChecker interface {
-	Subject() string
-	IsFree() bool
-}
-
-type HTTPStatusResponse struct {
-	Site       string
-	StatusCode int
-}
-
-func (res HTTPStatusResponse) IsFree() bool {
-	return res.StatusCode == 404
-}
-
-func (res HTTPStatusResponse) Subject() string {
-	return res.Site
-}
-
-type DNSStatusResponse struct {
-	DomainName string
-	IP         *net.IPAddr
-}
-
-func (res DNSStatusResponse) IsFree() bool {
-	return res.IP == nil
-}
-
-func (res DNSStatusResponse) Subject() string {
-	return res.DomainName
 }
 
 func main() {
-	wg := &sync.WaitGroup{}
-
-	chanRes := make(chan FreeChecker)
-
-	wg.Add(2)
-	go checkHTTPSites(wg, chanRes)
-	go checkDNSDomains(wg, chanRes)
-
+	// Generate 10 names
+	namesGenerator = make(chan string)
 	go func() {
-		wg.Wait()
-		close(chanRes)
+		for i := 0; i < 10; i++ {
+			namesGenerator <- generator.ConstructString(consonants, vowels)
+		}
+		close(namesGenerator)
 	}()
 
-	for checker := range chanRes {
-		if checker.IsFree() {
-			fmt.Println(checker.Subject(), "is free")
-		} else {
-			fmt.Println(checker.Subject(), "is occupied")
+	// Check availability of .com, twitter, facebook and github
+	mainWg := &sync.WaitGroup{}
+	for name := range namesGenerator {
+		nameChecker := &NameChecker{
+			Name:         name,
+			wg:           &sync.WaitGroup{},
+			chanCheckers: make(chan FreeChecker),
 		}
+		go nameChecker.check(mainWg)
+	}
+	mainWg.Wait()
+}
+
+func (nameChecker *NameChecker) check(mainWg *sync.WaitGroup) {
+	defer mainWg.Done()
+
+	mainWg.Add(1)
+	nameChecker.wg.Add(2)
+	go nameChecker.CheckDNS()
+	go nameChecker.CheckHTTP()
+
+	go func() {
+		nameChecker.wg.Wait()
+		close(nameChecker.chanCheckers)
+	}()
+
+	for freeChecker := range nameChecker.chanCheckers {
+		nameChecker.FreeCheckers = append(nameChecker.FreeCheckers, freeChecker)
+	}
+
+	fmt.Println(nameChecker.Name+" ? ", nameChecker.IsFree())
+}
+
+func (nameChecker *NameChecker) CheckDNS() {
+	defer nameChecker.wg.Done()
+
+	// Add DNS FreeChecker to chanCheckers
+	ip, err := net.ResolveIPAddr("ip", nameChecker.Name+".com")
+	if err != nil {
+		nameChecker.chanCheckers <- DNSStatusResponse{nameChecker.Name, nil}
+	} else {
+		nameChecker.chanCheckers <- DNSStatusResponse{nameChecker.Name, ip}
 	}
 }
 
-func checkHTTPSites(wg *sync.WaitGroup, chanRes chan<- FreeChecker) {
-	defer wg.Done()
+func (nameChecker *NameChecker) CheckHTTP() {
+	defer nameChecker.wg.Done()
 
-	for domain := range httpDomains {
-		for _, site := range sites {
-			wg.Add(1)
-			go func(site string) {
-				defer wg.Done()
-				url := fmt.Sprintf(site, domain)
-				res, err := http.Get(url)
-				if err != nil {
-					fmt.Printf("error %s %s %s\n", site, domain, err)
-					return
-				}
+	httpWg := &sync.WaitGroup{}
 
-				chanRes <- HTTPStatusResponse{Site: url, StatusCode: res.StatusCode}
-				res.Body.Close()
-			}(site)
-		}
-	}
-}
-
-func checkDNSDomains(wg *sync.WaitGroup, chanRes chan<- FreeChecker) {
-	defer wg.Done()
-
-	for domain := range dnsDomains {
-		wg.Add(1)
-		go func(domain string) {
-			defer wg.Done()
-			ip, err := net.ResolveIPAddr("ip", domain)
+	// Add HTTP FreeCheckers to chanCheckers
+	for _, site := range sites {
+		httpWg.Add(1)
+		go func(site string) {
+			defer httpWg.Done()
+			url := fmt.Sprintf(site, nameChecker.Name)
+			res, err := http.Get(url)
 			if err != nil {
-				chanRes <- DNSStatusResponse{domain, nil}
-			} else {
-				chanRes <- DNSStatusResponse{domain, ip}
+				fmt.Printf("error %s %s %s\n", site, nameChecker.Name, err)
+				return
 			}
-		}(domain)
+			nameChecker.chanCheckers <- HTTPStatusResponse{Site: url, StatusCode: res.StatusCode}
+			res.Body.Close()
+		}(site)
 	}
+
+	httpWg.Wait()
+}
+
+func (nameChecker *NameChecker) IsFree() bool {
+	for _, freeChecker := range nameChecker.FreeCheckers {
+		if !freeChecker.IsFree() {
+			return false
+		}
+	}
+	return true
 }
